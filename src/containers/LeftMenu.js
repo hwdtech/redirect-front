04b7e10bdd4a9596f85f91d20576460d37e9fb40
Vitkyk <pashcin2000@mail.ru>
2016-11-15 23:46:06 -0800

    Fixed input mode

--- src/containers/LeftMenu.js
+++ src/containers/LeftMenu.js
@@ -1,39 +1,44 @@
 import { connect } from 'react-redux'
-import { setVisibleContent, selectMainLink } from '../actions'
+import { setVisibleContent, selectMainLink, defaultInputMode } from '../actions'
 import * as CONSTANTS from '../components/CONSTANTS'
 import LeftMenuBar from '../components/LeftMenuBar'
 
 
+const setDefault = (e, dispatch) => {
+	e.preventDefault()
+	dispatch(defaultInputMode())
+}
+
 const mapStateToProps = (state) => {
-  return {
-  }
+	return {
+	}
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    onHomeClick: (e) => {
-      e.preventDefault()
-      dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_LIST]))
-      dispatch(selectMainLink(false))
-    },
-    onMainLinkFormClick: (e) => {
-      e.preventDefault()
-      dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_FORM]))
-    },
-    onMainLinkListClick: (e) => {
-      e.preventDefault()
-      dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_LIST]))
-    },
-    onSubLinkFormClick: (e) => {
-      e.preventDefault()
-      dispatch(setVisibleContent([CONSTANTS.SUB_LINK_FORM]))
-    }
-  }
+	return {
+		onHomeClick: (e) => {
+			dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_LIST]))
+			dispatch(selectMainLink(false))
+			setDefault(e, dispatch)
+		},
+		onMainLinkFormClick: (e) => {
+			dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_FORM]))
+			setDefault(e, dispatch)
+		},
+		onMainLinkListClick: (e) => {
+			dispatch(setVisibleContent([CONSTANTS.MAIN_LINK_LIST]))
+			setDefault(e, dispatch)
+		},
+		onSubLinkFormClick: (e) => {
+			dispatch(setVisibleContent([CONSTANTS.SUB_LINK_FORM]))
+			setDefault(e, dispatch)
+		}
+	}
 }
 
 const LeftMenu = connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(LeftMenuBar)
 
 export default LeftMenu
--- src/reducers/inputMode.js
+++ src/reducers/inputMode.js
@@ -6,8 +6,13 @@ const change_input_mode = (state, action) => {
 	return action[state]
 }
 
+const default_input_mode = (state, action) => {
+	return action.mode
+}
+
 let inputModeActions = {
 	CHANGE_INPUT_MODE: change_input_mode,
+	DEFAULT_INPUT_MODE: default_input_mode,
 }
 
 const inputMode = (state = 'ADD', action) => {
